545d04e25e8c50aea2c791e57f7e7b9d56c8404c
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-08-30 18:29:07 +0900

    man: ライブラリ分離

--- lib/xlib/xlib.dart
+++ lib/xlib/xlib.dart
@@ -1,42 +1,7 @@
-import "package:color/color.dart";
-import "package:nyxx/nyxx.dart" show DiscordColor;
-
 extension IterableFlatten<E> on Iterable<Iterable<E>>{
   Iterable<E> flatten() => this.expand((Iterable<E> e) => e);
 }
 
 extension CyclicIterable<E> on Iterable<E> {
   Iterable<E> sublistCyclic(int offset, int count) => Iterable<Iterable<E>>.generate((offset + count) ~/ this.length + 1, (_) => this).flatten().skip(offset).take(count);
-}
-
-extension DiscordColorConvert on Color {
-  DiscordColor forDiscord() {
-    RgbColor rgb = this.toRgbColor();
-    return DiscordColor.fromRgb(rgb.r.round(), rgb.g.round(), rgb.b.round());
-  }
-}
-
-final class Colors {
-  const Colors();
-  static Color get ruwi => RgbColor(136, 0, 0);
-  static Color get marbleBasic => 0xd3.toRgbColor;
-  static Color get marbleLight => 0xef.toRgbColor;
-  static Color get marbleDark => 0xaa.toRgbColor;
-  static Color get marble1 => Colors.marbleBasic;
-  static Color get marble2 => Colors.marbleLight;
-  static Color get marble3 => Colors.marbleDark;
-  static Color get marbleGrue => HexColor("a2b1a4");
-  static Color marble(int variant) => switch (variant){
-    1 => Colors.marble1,
-    2 => Colors.marble2,
-    3 => Colors.marble3,
-    _ => Colors.marble((variant - 1) % 3 + 1),
-  }
-  static Color get grue => HexColor("36c8b5");
-}
-
-const colors = Colors();
-
-extension ColorTriplet on num {
-  Color get toRgbColor => RgbColor(this, this, this);
 }
