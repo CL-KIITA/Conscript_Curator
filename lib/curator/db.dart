412b00b69af7d99cdb65b074a080e79915e6eb3b
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-09-03 19:15:41 +0900

    add: データベース行抽象オブジェクト

---
+++ lib/curator/db.dart
@@ -0,0 +1,48 @@
+import "package:nyxx/nyxx.dart";
+import "package:gsheets/gsheets.dart";
+import "package:intl/locale.dart";
+import "package:language_code/language_code.dart"
+
+import "package:conscript_curator/xlib/general.dart";
+import "package:conscript_curator/curator/kernel.dart";
+import "package:conscript_curator/curator/sheet.dart";
+import "package:conscript_curator/curator/massage.dart";
+
+class CXSRecord {
+  final String example;
+  final String lang;
+  final String? translated;
+  final String? contributor;
+  
+  CXSRecord({
+    required this.example,
+    Locale? lang,
+    this.translated,
+    this.contributor}):
+    this.lang = lang ?? CXSRecord.defaultLang;
+  
+  List<EmbedFieldBuilder> get fields {
+    List<EmbedFieldBuilder> fs = <EmbedFieldBuilder>[
+      EmbedFieldBuilder(name: Paren.guil.cover("例文"), value: this.example, isInline: true),
+      EmbedFieldBuilder(name: Paren.guil.cover(""), value: LanguageCodes.fromCode(this.lang.toString()).nativeName, isInline: true)];
+    
+    if (this.translated != null) {
+      fs.add(EmbedFieldBuilder(name: Paren.guil.cover("翻訳"), value: this.translated, isInline: false));
+    }
+    
+    if (this.contributor != null) {
+      fs.add(EmbedFieldBuilder(name: Paren.guil.cover("提供"), value: this.contributor, isInline: this.translated != null));
+    }
+    return fs;
+  }
+  
+  static Future<CXSRecord> fetch(CuratorSystem system) async {
+    return CXSRecord.beta;
+  }
+  
+  static CXSRecord get beta = CXSRecord(
+    example: "愛・希望　君の言葉を　この胸に$nl　今日を明日を　歩みゆく",
+  　lang: ,
+  　contributor: "佐藤 陽花");
+  static Locale defaultLang => Locale.parse("ja");
+}
