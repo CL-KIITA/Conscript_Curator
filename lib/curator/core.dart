db36254097142e538ff2c0dfc0c062094519c11c
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-09-03 19:18:20 +0900

    ud: db.dartのエクスポート追加

--- lib/curator/core.dart
+++ lib/curator/core.dart
@@ -2,3 +2,4 @@ export "package:conscript_curator/curator/ref.dart";
 export "package:conscript_curator/curator/kernel.dart";
 export "package:conscript_curator/curator/massage.dart";
 export "package:conscript_curator/curator/sheet.dart";
+export "package:conscript_curator/curator/db.dart";
