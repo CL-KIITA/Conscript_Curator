a0c3126267c50bd8547825305fc66d98f5b89060
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-09-06 15:54:21 +0900

    add: db.dartのexport追加

--- lib/curator/core.dart
+++ lib/curator/core.dart
@@ -1,5 +1,5 @@
 export "package:conscript_curator/curator/ref.dart";
-export "package:conscript_curator/curator/kernel.dart";
+export "package:conscript_curator/curator/kernel.SheetLabel
 export "package:conscript_curator/curator/massage.dart";
 export "package:conscript_curator/curator/sheet.dart";
 export "package:conscript_curator/curator/db.dart";
