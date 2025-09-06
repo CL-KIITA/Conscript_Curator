6700342462dc34cf61bbb4aa8b10ad2e31463cd5
halka9000stg <as.he.that.doth.serve@gmail.com>
2025-09-06 15:45:43 +0900

    add: Range

--- lib/xlib/collection.dart
+++ lib/xlib/collection.dart
@@ -5,3 +5,10 @@ extension IterableFlatten<E> on Iterable<Iterable<E>>{
 extension CyclicIterable<E> on Iterable<E> {
   Iterable<E> sublistCyclic(int offset, int count) => Iterable<Iterable<E>>.generate((offset + count) ~/ this.length + 1, (_) => this).flatten().skip(offset).take(count);
 }
+
+extension type Range._(List<int> elements) implements List<int> {
+  int get first => this.elements.first;
+  int get last => this.elements.last;
+  int get count => this.elements.length;
+  Range({int? first, int? last, int? Interval}): this._(List<int>.generate(((last ?? (first ?? 1) + 1) - (first ?? 1)) ~/ (interval ?? 1), (int i) => (first ?? 1) + (i * (interval ?? 1  ))));
+}
